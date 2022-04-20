4bae22880451b73a1ba6f3b228b3a8648e5e0e31
AKSarder <63703796+AKSarder@users.noreply.github.com>
2022-04-20 09:43:37 +0600

    done

---
+++ output/script.js
@@ -0,0 +1,13 @@
+"use strict";
+// console.log("I love Bangladesh");
+// let playerName = "Mohammad";
+// let playerName2;
+// playerName2 = "Osman";
+// playerName2 = 40;
+// console.log(playerName);
+// // playerName = 235; error
+// console.log(playerName2)
+function add(a, b) {
+    return a * b;
+}
+console.log(add());
